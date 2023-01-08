9ba621bdb5e23b7b8fb7a7bc84e7500cc1a79c6e
jjdonnel <jdonnelly71@yahoo.com>
2023-01-07 20:40:07 -0500

    dotenv

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-    require("DOTENV").config();
+    require("dotenv").config();
 };
 const cloudinary = require('cloudinary').v2;
 const express = require('express');
